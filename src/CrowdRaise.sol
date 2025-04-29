ab2dd7b13b57737cbade304ebbabbbf8297f9baa
Salman Izzuddin <salmanizzu.dev@protonmail.com>
2025-04-29 12:23:30 +0700

    contract: initialize contract, add deployment, add initial test

---
+++ src/CrowdRaise.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract CrowdRaise {
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) private s_addressToAmountFunded;
+
+    address[] private s_funders;
+    uint256 public s_totalFunded;
+    address private immutable i_owner;
+    uint256 private immutable i_usdGoal;
+    uint256 private immutable i_deadline;
+    AggregatorV3Interface private s_priceFeed;
+
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+    uint256 public constant SECOND_TO_DAY = 24 * 60 * 60;
+
+    event Funded(address indexed funder, uint256 amount);
+
+    constructor(address priceFeed, uint256 goalUsdAmount, uint256 durationInDays) {
+        require(goalUsdAmount >= 100 * 10 ** 18);
+        require(goalUsdAmount <= 1_000_000 * 10 ** 18);
+        i_owner = msg.sender;
+        i_usdGoal = goalUsdAmount;
+        i_deadline = block.timestamp + (durationInDays * SECOND_TO_DAY);
+        s_priceFeed = AggregatorV3Interface(priceFeed);
+    }
+
+    function fund() public payable {
+        require(block.timestamp <= i_deadline, "Deadline passed!");
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need more ETH!");
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_totalFunded += msg.value;
+        s_funders.push(msg.sender);
+
+        emit Funded(msg.sender, msg.value);
+    }
+}
---
+++ test/unit/CrowdRaiseTest.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {DeployCrowdRaise} from "../../script/DeployCrowdRaise.s.sol";
+import {CrowdRaise} from "../../src/CrowdRaise.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+contract CrowdRaiseTest is Test {
+    CrowdRaise crowdRaise;
+
+    uint256 public constant SEND_VALUE = 0.01 ether;
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    uint160 public constant USER_NUMBER = 46;
+    address public constant USER = address(USER_NUMBER);
+
+    function setUp() external {
+        DeployCrowdRaise deployCrowdRaise = new DeployCrowdRaise();
+        crowdRaise = deployCrowdRaise.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testMinimumFundIsFiveUsd() public view {
+        assertEq(crowdRaise.MINIMUM_USD(), 5e18);
+    }
+}
